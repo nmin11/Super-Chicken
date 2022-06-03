24e22ed7aad92103646ed70a1426e30c2f98bcf5
nmin11 <nmin1124@gmail.com>
2022-06-03 17:43:57 +0900

    Initial commit

---
+++ src/main/java/com/SuperChicken/SuperChickenApplication.java
@@ -0,0 +1,13 @@
+package com.SuperChicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperChickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperChickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SuperChicken/SuperChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SuperChicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperChickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
