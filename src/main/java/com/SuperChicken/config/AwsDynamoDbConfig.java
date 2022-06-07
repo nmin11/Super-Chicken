c30f8cb946f2b1585510ed59fb68133456fb193d
nmin11 <nmin1124@gmail.com>
2022-06-07 17:24:58 +0900

    Edit : aws credential to use IAM role

--- src/main/java/com/SuperChicken/config/AwsDynamoDbConfig.java
+++ src/main/java/com/SuperChicken/config/AwsDynamoDbConfig.java
@@ -1,16 +1,12 @@
 package com.SuperChicken.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -18,21 +14,6 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 @EnableDynamoDBRepositories(basePackages = {"com.SuperChicken.dynamodb"})
 public class AwsDynamoDbConfig {
-    @Value("${aws.access_key}")
-    private String access_key;
-
-    @Value("${aws.secret_key}")
-    private String secret_key;
-
-    public AWSCredentialsProvider awsCredentialsProvider() {
-        return new AWSStaticCredentialsProvider(awsCredentials());
-    }
-
-    @Bean
-    public AWSCredentials awsCredentials() {
-        return new BasicAWSCredentials(access_key, secret_key);
-    }
-
     @Bean
     @Primary
     public DynamoDBMapperConfig dynamoDBMapperConfig() {
@@ -48,7 +29,7 @@ public class AwsDynamoDbConfig {
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(awsCredentialsProvider())
+                .withCredentials(InstanceProfileCredentialsProvider.getInstance())
                 .withRegion(Regions.AP_NORTHEAST_2)
                 .build();
     }
